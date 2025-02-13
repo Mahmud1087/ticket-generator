09850d724bde675025e92f0229cf53c5d20d9f18
Mahmud1087 <abdulazeezm578@gmail.com>
2025-02-13 16:56:47 +0100

    Feat: Step two added

--- src/config/static.ts
+++ src/config/static.ts
@@ -1,2 +1,5 @@
 export const LOGO_IMG = 'logo.png';
 export const CLOUD_DOWNLOAD_IMG = 'cloud-download.png';
+export const BAR_CODE = 'bar-code.png';
+export const BARS = 'bars.png';
+export const BG = 'bg.png';
